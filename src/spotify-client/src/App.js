fa763283e1d9c6399479809d1a7e8a017178f6b7
syang98 <syang199@gmail.com>
2020-08-20 16:16:43 -0400

    Organize folders

--- src/spotify-client/src/App.js
+++ src/spotify-client/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import Cookies from 'js-cookie';
 import './App.css';
 
 class App extends Component{
   constructor() {
     super();
-    this.refresh_token = "Not logged in"
+    this.state = {
+      access_token: Cookies.get('access_token'), 
+      refresh_token: Cookies.get('refresh_token')
+    }
   }
 
-  async componentDidMount() {
-    
-  }
+
 
   
   render() { return (
@@ -18,7 +20,7 @@ class App extends Component{
           <button>Login to Spotify</button>
         </a> 
         <h1>
-          Refresh Token: {this.refresh}
+          Refresh Token: {this.state.refresh_token}
         </h1>
       </div>
     );
